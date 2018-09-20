a7df4c8ead16cec741ac0f971216c6bea7df7f4a
Joshua Brown <josh@mjbrowns.com>
2018-09-20 19:59:00 -0400

    making String file for reference & renaming of file

---
+++ Strings.py
@@ -0,0 +1,23 @@
+firstCall = input("Please Enter a string!\n::\t")
+def stringEx(UserIn):
+    print(UserIn)
+    print(UserIn.upper())
+    print(UserIn.lower())
+    print(UserIn[::-1])
+    for i in UserIn:
+        print(i)
+    print("------")
+    for i in UserIn[::-1]:
+        print(i)
+    print("------")
+    print("This is a formated string {}".format(UserIn))
+    print("This is not a formated string " + UserIn)
+    print(UserIn.split(UserIn[0])) #RETURNS ARRAY
+    print(UserIn.strip()) #Removes white space from front and back
+    print(len(UserIn)) #Returns # of charaters in string
+    print(UserIn.replace(UserIn[0], UserIn[1])) #replaces things
+    
+
+
+
+stringEx(firstCall)
