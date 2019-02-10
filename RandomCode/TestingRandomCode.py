acf97225ad5f30026e8ab1079f95e0d7a5559c23
Joshua Brown <josh@mjbrowns.com>
2019-02-09 21:02:52 -0500

    Began working on file sorter.

--- RandomCode/TestingRandomCode.py
+++ RandomCode/TestingRandomCode.py
@@ -1,16 +1,5 @@
-import sys
-import time
-toolbar_width = 40
-
-# setup toolbar
-sys.stdout.write("[{}]".format(" " * toolbar_width))
-sys.stdout.flush()
-sys.stdout.write("\b" * (toolbar_width+1)) # return to start of line, after '['
-
-for i in range(toolbar_width):
-    time.sleep(0.1) # do real work here
-    # update the bar
-    sys.stdout.write("#")
-    sys.stdout.flush()
-
-sys.stdout.write("\n")
+import os
+for folderName, subfolders, filenames in os.walk("E:\\01331 (Title-Artist) (8-GB)"):
+    #print(folderName+"\\"+subfolders+"\\"+filenames)
+    for file in filenames:
+        print(folderName+"\\"+file)
--- WorkPrograms/FileSorter.py
+++ WorkPrograms/FileSorter.py
@@ -1,11 +1,61 @@
+import string
 import os
+import tempfile
 import shutil
+import sys
 def WhereFilePath():
-    global FilePath
-    FilePath = input("Where is the file you would like to sort?\n-> ")
+    global FilePathInput
+    #FilePath = input("Where is the file you would like to sort?\n-> ")
+    FilePathInput = "E:\\TEST1331"
+# Book maker must be seperable from file sorter! Also, File FileSorter
+#    Must have option to Not create sub folders
+#FileList = list(string.ascii_lowercase)
+def FileSorter(FilePath):
 
-def FileSorter():
-    pass
-    
+    """
+    SOLVED
+
+    Create array of file paths that need to be moved, then move that array to specefied folder inside origin, then sort.
+    also, Create folder in same area as specefied folder, then create its sub folders as needed as transfer of songs start.
+
+    """
+
+
+
+
+
+    tmpfolder = FilePath+"\\TMPSONGHOLD"
+    shutil.rmtree(tmpfolder)
+    os.mkdir(tmpfolder)
+    for folderName, subfolders, filenames in os.walk(FilePath):
+        loadBar(filenames)
+        count = 0
+        for file in filenames:
+            count += percent
+            if count >= 1:
+                count = 0
+                moveBar()
+            #print(folderName+"\\"+file)
+            if ".cdg" or ".mp3" in file:
+                try:
+                    shutil.move(folderName+"\\"+file,tmpfolder)
+                except:
+                    print("Moving of "+folderName+"\\"+file+ " Failed!")
+    print("\nDone!")
+
+
+def loadBar(size):
+    global length
+    length = 30
+    global percent
+    percent = length/len(size)
+    #print("Writing to file")
+    sys.stdout.write("[{0}]".format(" " * (length)))
+    sys.stdout.flush()
+    sys.stdout.write("\b" * (length))
+def moveBar():
+    sys.stdout.write('\b->')
+    sys.stdout.flush()
 
 WhereFilePath()
+FileSorter(FilePathInput)
