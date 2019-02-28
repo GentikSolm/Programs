d9f099a7bf24c21fbc939a59b2232a58b58b8c1e
Joshua Brown <josh@mjbrowns.com>
2019-02-28 14:51:35 -0500

    new tests

--- RandomCode/TestingRandomCode.py
+++ RandomCode/TestingRandomCode.py
@@ -1,10 +1,2 @@
-import os
-
-for folderName, subfolders, filenames in os.walk("E:\\TEST1331"):
-    for file in filenames:
-        if ".cdg" in file.lower() or ".mp3" in file.lower():
-            #sortedSongList.append(folderName+"\\"+file)
-            print("-- "+file)
-        elif ".cdg" or ".mp3" not in file.lower():
-            #ExtraFiles.append(folderName+"\\"+file)
-            print("!! "+file)
+test = "/"
+print(test.upper())
