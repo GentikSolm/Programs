e118a96821882a785f9c744d5733eefd14b8b0ee
Josh <josh@mjbrowns.com>
2019-04-11 14:11:04 -0400

    Finished file sorter

--- WorkPrograms/FileSorter.py
+++ WorkPrograms/FileSorter.py
@@ -3,7 +3,7 @@ import string, os, shutil, sys
 def WhereFilePath():
     global FilePathInput
     #FilePath = input("Where is the file you would like to sort?\n-> ")
-    FilePathInput = "E:\\TEST1331"
+    FilePathInput = "D:\\Testing\\01331 (Title-Artist) (8-GB) Origional"
 # Book maker must be seperable from file sorter! Also, File FileSorter
 #    Must have option to Not create sub folders
 #FileList = list(string.ascii_lowercase)
@@ -12,18 +12,12 @@ def parseInputs(Path, subFiles):
     #Make sure all inputs are good. Dont want to move the wrong folder / things
     pass
 def GetUpperFilePath(PathInput):
-    global ListupperFilePath
     ListupperFilePath = PathInput.split("\\")
-    global OrigionalFolderName
     OrigionalFolderName = ListupperFilePath.pop()
-    global UpperFilePath
+    #UpperFilePath = ListupperFilePath[-1]
     UpperFilePath = ListupperFilePath[0]
     for i in ListupperFilePath[1:]:
         UpperFilePath +="\\"+i
-
-    # if ListupperFilePath[0] == ListupperFilePath[-2]:
-    #     return ListupperFilePath[-2]+"\\"
-    # else:
     return UpperFilePath + "\\New" + OrigionalFolderName
 def FileSorter(FilePath,ReqSub):
     sortedSongList = []
@@ -35,10 +29,12 @@ def FileSorter(FilePath,ReqSub):
         return
     print("Loading songs into List")
     #countForBreak = 0
-    for folderName, subfolders, filenames in os.walk(FilePath):
+    for (folderName, subfolders, filenames) in os.walk(FilePath):
         sys.stdout.write("\n")
         percentCount = loadBar(filenames)
         count = 0
+        subfolders.sort()
+        tmp = []
         for file in filenames:
             #Moving Progress bar
             count += 1
@@ -47,65 +43,76 @@ def FileSorter(FilePath,ReqSub):
                 moveBar()
             #Adding files to sortedSongList
             if ".cdg" in file.lower() or ".mp3" in file.lower():
-                sortedSongList.append(folderName+"\\"+file)
-            elif ".cdg" not in file.lower() or ".mp3" not in file.lower():
+                sortedSongList.append(os.path.join(folderName, file))
+            else:
                 ExtraFiles.append(folderName+"\\"+file)
     copyToFolders(sortedSongList, ReqSub, FilePath, ExtraFiles)
 
     print("\nDone!")
 def copyToFolders(myList, subFoldersNeeded, FilePath, anyExtraFiles):
-    myList.sort()
-#NEED TO FINISH THESE STATEMENTS!!!!!!!!!!!!!! FILE SORTING
+    myList.sort(key=str.lower)
     if subFoldersNeeded == True:
-        Letter = None
         maxFolderSize = 1000
         currentSize = 0
         currentnum = 0
-        currentFolder = GetUpperFilePath(FilePath) + "\\" + Letter
+        currentFolder = GetUpperFilePath(FilePath)
         lastfile = myList[0]
-        for file in myList:
+        percentCount = loadBar(myList)
+        count = 0
+        for filewp in myList:
+            file = filewp.split("\\")[-1]
             currentSize += 1
             #Places file in folder with same letter if letter is the same as previous file
-            if file[0].upper() == lastfile[0].upper():
+            if file[0].upper() == lastfile[0].upper() and file[0].lower() in list(string.ascii_lowercase) or file[0].upper() == lastfile[0].upper() and file[0] in list("123456789"):
                 Letter = file[0].upper()
                 if currentSize >= maxFolderSize:
                     currentnum += 1
-                    currentSize = 1
+                    currentSize = 0
                 if currentnum != 0:
-                    Letter = Letter + " - " + currentnum
-                copyFileFunc(,currentFolder)
-            #Handles files that do not start with a Letter
-            elif file[0].upper() not in list(string.ascii_lowercase):
+                    Letter = Letter[0] + " - " + str(currentnum)
+                currentSize +=1
+                    #Handles files that do not start with a Letter
+                #Changes letter and resets counter
+            elif file[0].upper() != lastfile[0].upper():
+                currentSize = 0
+                currentnum = 0
+                Letter = file[0].upper()
+                #while currentSize <= maxFolderSize:
+            else:
                 Letter = "0"
                 if currentSize >= maxFolderSize:
                     currentnum += 1
                     currentSize = 0
                 if currentnum != 0:
-                    Letter = Letter + " - " + currentnum
-            #Changes letter and resets counter
-            elif file[0].upper() != lastfile[0].upper():
-                currentSize = 0
-
+                    Letter = Letter[0] + " - " + str(currentnum)
+                    currentSize += 1
+            newfolder = currentFolder + "\\" + Letter
+            try:
+                os.mkdir(newfolder)
+            except:
+                pass
+            shutil.copy(filewp,newfolder)
             lastfile = file
-                #while currentSize <= maxFolderSize:
+            count += 1
+            if percentCount != "small" and count >= percentCount:
+                count = 0
+                moveBar()
+
     elif subFoldersNeeded == False:
         sys.stdout.write("\nWriting ")
         for i in myList:
             loadFileName(i)
-            shutil.move(i, GetUpperFilePath(FilePath))
+            shutil.copy(i, GetUpperFilePath(FilePath))
         if len(anyExtraFiles) != 0:
             sys.stdout.write("\nWriting ")
             os.mkdir(GetUpperFilePath(FilePath) + "\\EXTRAFILES")
             for j in anyExtraFiles:
                 loadFileName(j)
-                shutil.move(j, GetUpperFilePath(FilePath) + "\\EXTRAFILES")
+                shutil.copy(j, GetUpperFilePath(FilePath) + "\\EXTRAFILES")
             sys.stdout.write("\nEXTRA FILES FOUND, PLEASE REMOVE FROM NEW FOLDER")
     else:
         print("Im sorry, i dont understand!")
         pass
-def copyFileFunc(thisFolder, thisFile):
-    shutil.copy(thisFile, thisFolder)
-    return
 #Functions for Progress bar, not intended for final use.
 def loadFileName(name):
     sys.stdout.write(name)
@@ -131,4 +138,4 @@ def moveBar():
 
 
 WhereFilePath()
-FileSorter(FilePathInput,False)
+FileSorter(FilePathInput,True)
