553848db55d01cf7f5cf0d1624bdfd4c8dbf9d92
Joshua Brown <josh@mjbrowns.com>
2019-09-05 13:17:42 -0400

    added beginning of help function to contact book prog

---
+++ ClassProjects/ProjectADay/ContactBook.py
@@ -0,0 +1,22 @@
+import time
+
+
+#Simple program that takes inputs and addes them to an address book
+print("Welcome to the adress book. Please type 'Help' for a list of commands")
+'''
+List of commands that need made:
+    Adding item to address book
+    Deleting item from address book
+    Editing item in address book
+    List all contacts
+    Seek contact list
+    Search by field
+
+
+List of functions that are non-command oriented:
+    Create a function to parse inputs and clean them up if not good for required field
+    save function to acces and manage contacts in seperate .json file.
+
+'''
+while True:
+    pass
